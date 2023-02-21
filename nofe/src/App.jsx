c24a125e76264a9a85dedb5e36579a05b28c6d1d
Vytautas Valutis <dnc908@gmail.com>
2023-02-21 14:56:37 +0200

    Work

--- nofe/src/App.jsx
+++ nofe/src/App.jsx
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Bebras />
+        <Bebras color="yellow" name="Alfa"/>
+        <Bebras color="cyan"  name="Beta"/>
       </header>
     </div>
   );
--- nofe/src/Components/003/Bebras.jsx
+++ nofe/src/Components/003/Bebras.jsx
@@ -1,7 +1,25 @@
-function Bebras() {
+
+import rand from "../../Functions/rand";
+
+function Bebras({color, name}) {
+
+    const fun = () => {
+        let a= '';
+        for (let i = 1; i < 9; i++) {
+            a += i;
+        }
+        return a;
+    }
+
     return <>
-        <h1> Bebras </h1>
-        <h2> <i>Barsukas</i> </h2>
+        <h1 style={{
+            color: color,
+            backgroundColor: rand(0,1) ? 'coral' : 'pink'
+        }}> Bebras - {name}</h1>
+        <h2 className="red"> <i>Barsukas ind </i> {
+            fun()
+        }    
+        </h2>
     </>
 }
 
