a5f7844319e28654cdf004b1c24918550a284526
Vytautas Valutis <dnc908@gmail.com>
2023-02-21 18:08:25 +0200

    Object

--- nofe/src/App.jsx
+++ nofe/src/App.jsx
@@ -1,12 +1,34 @@
-import './App.css';
-import Bebras from './Components/003/Bebras';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+
+
+const animals = [
+{name :'Racoon', color: 'skyblue', big: false},  
+{name :'Fox', color: 'brown', big: false},  
+{name :'Moose', color: 'yellow', big: true},  
+{name :'Wolf', color: 'gray', big: false}
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Bebras color="yellow" name="Alfa"/>
-        <Bebras color="cyan"  name="Beta"/>
+
+        <div className="card">
+          <div className="card-header">
+            <h2>Forest book</h2>
+          </div>
+          <ul className="list-group list-group-flush">
+            {
+              animals.map((a, i) => <li key={i} className={'list-group-item' + (a.big ? ' big' : '')} 
+              style={{color: a.color}}>
+                {a.name}
+                </li> )
+            }
+            
+          </ul>
+        </div>
+
       </header>
     </div>
   );
