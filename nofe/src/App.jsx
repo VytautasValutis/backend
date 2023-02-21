4c3b5694de47a866835321a7c07f242ccf1410c9
Vytautas Valutis <dnc908@gmail.com>
2023-02-21 14:28:19 +0200

    React, Bebras

---
+++ nofe/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Bebras from './Components/003/Bebras';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Bebras />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ nofe/src/Components/003/Bebras.jsx
@@ -0,0 +1,8 @@
+function Bebras() {
+    return <>
+        <h1> Bebras </h1>
+        <h2> <i>Barsukas</i> </h2>
+    </>
+}
+
+export default Bebras;
