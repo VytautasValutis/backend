6b58948c97edf0abb0f62326b8f6d8078e5329b3
Vytautas Valutis <dnc908@gmail.com>
2023-02-22 23:26:31 +0200

    HW #6 react start

--- nofe/src/App.jsx
+++ nofe/src/App.jsx
@@ -3,61 +3,29 @@ import { useState } from 'react';
 import './App.scss';
 import { v4 as uuidv4 } from 'uuid';
 
-class ColorBall {
-  constructor(color, number) {
-    this.color = color;
-    this.number = number;
-  }
-}
-
 function App() {
   console.clear();
 
-  const [balls, setBalls] = useState([]);
-  const [ballColor, setBallColor] = useState('white');
-  const [ballNumber, setBallNumber] = useState(0);
+  const [lBalls, setLBalls] = useState([]);
+  const [rBalls, setRBalls] = useState([]);
 
-  const doBallColor = e => {
-    setBallColor(e.target.value);
-  }
-  const doBallNumber = e => {
-    setBallNumber(e.target.value);
-  }
+const reset = () => {
 
-  const add = () => {
-    setBalls(b => [...b, {
-      id: uuidv4(),
-      color: ballColor,
-      number: ballNumber
-    }]);
-  }
-  const del = () => {
-    setBalls(b => b.filter(w => balls[0].id !== w.id));
-  }
+};
 
   return (
     <div className="App">
       <header className="App-header">
-        <div className="container">
-          <div className="row">
-            <div className="col-3">
-              <input type="text" className="form-control" onChange={doBallColor} value={ballColor} />
-            </div>
-            <div className="col-3">
-              <input type="text" className="form-control" onChange={doBallNumber} value={ballNumber} />
-            </div>
-            <div className="col-3">
-              <button type="button" className="btn btn-outline-success" onClick={add}>ADD new</button>
-            </div>
-            <div className="col-3">
-              <button type="button" className="btn btn-outline-danger" onClick={del}>DELETE oldest</button>
-            </div>
-          </div>
+        <button type="button" className="btn btn-outline-danger" onClick={reset}>RESET</button>
+        <div className="desk">
+        <div className="ball-block">
+          {
+          }
         </div>
         <div className="ball-block">
           {
-            balls.map((a, i) => <div key={i} className="balls" style={{ backgroundColor: a.color }}>{a.number}</div>)
           }
+        </div>
         </div>
       </header>
     </div>
