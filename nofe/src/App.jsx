47b213bfacfaecde65f5787f89353296f7225a00
Vytautas Valutis <dnc908@gmail.com>
2023-02-26 13:59:13 +0200

    HW8002

--- nofe/src/App.jsx
+++ nofe/src/App.jsx
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 import './App.scss';
 import './Components/HW/HW8001.scss';
+import './Components/HW/HW8002.scss';
 import HW8001 from './Components/HW/HW8001';
+import HW8002 from './Components/HW/HW8002';
 
 function App() {
 
   return (
-    <HW8001 />
+    <HW8002 />
   );
 }
 
