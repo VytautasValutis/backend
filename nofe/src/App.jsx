f555a5b5b054502607d056088e2a17190d14f7d4
Vytautas Valutis <dnc908@gmail.com>
2023-02-26 14:26:14 +0200

    HW8003

--- nofe/src/App.jsx
+++ nofe/src/App.jsx
@@ -3,13 +3,15 @@ import { useState } from 'react';
 import './App.scss';
 import './Components/HW/HW8001.scss';
 import './Components/HW/HW8002.scss';
+import './Components/HW/HW8003.scss';
 import HW8001 from './Components/HW/HW8001';
 import HW8002 from './Components/HW/HW8002';
+import HW8003 from './Components/HW/HW8003';
 
 function App() {
 
   return (
-    <HW8002 />
+    <HW8003 />
   );
 }
 
