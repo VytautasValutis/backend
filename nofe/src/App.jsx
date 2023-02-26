b08a29f82d7a8ec106149083edec38b648591c56
Vytautas Valutis <dnc908@gmail.com>
2023-02-26 15:20:26 +0200

    HW8005

--- nofe/src/App.jsx
+++ nofe/src/App.jsx
@@ -1,19 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 import './App.scss';
-import './Components/HW/HW8001.scss';
-import './Components/HW/HW8002.scss';
-import './Components/HW/HW8003.scss';
 import './Components/HW/HW8004.scss';
-import HW8001 from './Components/HW/HW8001';
-import HW8002 from './Components/HW/HW8002';
-import HW8003 from './Components/HW/HW8003';
+import './Components/HW/HW8005.scss';
 import HW8004 from './Components/HW/HW8004';
+import HW8005 from './Components/HW/HW8005';
 
 function App() {
 
   return (
-    <HW8004 />
+    <HW8005 />
   );
 }
 
