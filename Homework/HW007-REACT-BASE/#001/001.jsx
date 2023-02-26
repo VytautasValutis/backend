0d6ff4b22751ec006b6026afb257d5c928c8fa8b
Vytautas Valutis <dnc908@gmail.com>
2023-02-26 10:40:07 +0200

    HW

---
+++ Homework/HW007-REACT-BASE/#001/001.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import './App.scss';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p style={{color: 'red'}}>LABAS ZUIKI!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
